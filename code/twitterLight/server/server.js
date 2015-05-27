9b1b154e4b6cbda5f17fafe36a43efeadcec0a3d
Philip Peter <github@justgeek.de>
2015-05-27 22:56:23 +0200

    Started on hashtag count

--- code/twitterLight/server/server.js
+++ code/twitterLight/server/server.js
@@ -1,18 +1,32 @@
 var http = require('http');
 var router = require('node-simple-router')({list_dir: false});
+var url = require('url') ;
 
 var Twit = require('twit')
 var config = require('./config.js')
 
 var T = new Twit(config.twitter)
-T.get('search/tweets', { q: 'banana since:2011-11-11', count: 100 }, function(err, data, response) {
-  console.log(data)
-})
-
 
 router.get("/hello", function(request, response) {response.end("Hello, World!");});
 router.get("/sinceId", function(request, response) {
-  response.end("Hello, World!");
+  var queryObject = url.parse(request.url,true).query;
+
+  var result = 0;
+  var startId = queryObject['id'];
+  var tag = queryObject['tag'];
+
+  if(typeof startId === 'undefined') {
+    startId = 0;
+  }
+
+  if(typeof tag !== 'undefined') {
+    T.get('search/tweets', { q: tag, since_id:startId, count: 2 }, function(err, data, res) {
+      console.log(data['statuses'].length);
+      response.end('len: ' + data['statuses'].length);
+    });
+  } else {
+    response.end("0");
+  }
 });
 
 http.createServer(router).listen(1187);
