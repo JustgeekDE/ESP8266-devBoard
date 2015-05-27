c5aee40a0c17bd089a4e6b7d357b76a452ba6092
Philip Peter <github@justgeek.de>
2015-05-27 21:54:14 +0200

    playing around with twitter api
    adding debug logs to gitignore

--- code/twitterLight/server/server.js
+++ code/twitterLight/server/server.js
@@ -1,7 +1,18 @@
 var http = require('http');
 var router = require('node-simple-router')({list_dir: false});
 
+var Twit = require('twit')
+var config = require('./config.js')
+
+var T = new Twit(config.twitter)
+T.get('search/tweets', { q: 'banana since:2011-11-11', count: 100 }, function(err, data, response) {
+  console.log(data)
+})
+
+
 router.get("/hello", function(request, response) {response.end("Hello, World!");});
-router.get("/hello", function(request, response) {response.end("Hello, World!");});
+router.get("/sinceId", function(request, response) {
+  response.end("Hello, World!");
+});
 
 http.createServer(router).listen(1187);
